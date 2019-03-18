b7e4371814d0b16bf91b5e08b48849ed8bcb8b11
Edder Ramírez <edder.ramirez@vordem.mx>
2019-03-18 14:11:55 -0600

    static

--- oxxos/oxxos/settings.py
+++ oxxos/oxxos/settings.py
@@ -125,10 +125,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(
-        BASE_DIR,
-        'static/'
-    )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = os.environ.get('MEDIA_URL', '/media/')
